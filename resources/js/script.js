98eadc424ef6d1cd79dec3e934a47a95eee51979
barak8989 <40257379+barak8989@users.noreply.github.com>
2020-06-16 15:56:28 +0300

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        
+                anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+                menu: '#menu',
+                css3: true,
+                scrollingSpeed: 1000,
+                
+            });
+  
+        $(".ft").fadeIn(1000)  
+       
+});
+
